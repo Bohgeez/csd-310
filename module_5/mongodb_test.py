3a3d65554407011140c75f3b812640f1fd4e98a5
bohgeez <78043469+bohgeez@users.noreply.github.com>
2022-06-21 22:11:23 -0600

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,5 +1,10 @@
+"""gets pytech cluster"""
 from pymongo import MongoClient
-url = "mongodb+srv://admin:admin@cluster0.p8dde.mongodb.net/pytech"
-client = MongoClient(url)
+
+URL = "mongodb+srv://admin:admin@cluster0.p8dde.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(URL)
+
 db = client.pytech
+
 print(db.list_collection_names)
