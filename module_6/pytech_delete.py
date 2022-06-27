f59fa7346f3161bf5b8af3c89741ac14578d5b35
bohgeez <78043469+bohgeez@users.noreply.github.com>
2022-06-26 21:22:04 -0600

    Update pytech_delete.py

--- module_6/pytech_delete.py
+++ module_6/pytech_delete.py
@@ -10,28 +10,29 @@ db = conn.pytech
 
 collection = db.students
 
-#Call the find() method and display the results to the terminal window.
+# Call the find() method and display the results to the terminal window.
 cursor = collection.find()
 for record in cursor:
     print(record)
 
-#Call the insert_one() method and Insert a new document into the pytech collection with student_id 1010
+# Call the insert_one() method and Insert a new document into the pytech
+# collection with student_id 1010
 cursor = collection.find_one()
 for record in cursor:
     print(record)
     result = collection.insert_one({"student_id": 1010},
     {"$set":{"last_name": "Smith"}})
 
-#Call the find_one() method and display the results to the terminal window.
+# Call the find_one() method and display the results to the terminal window.
 cursor = collection.find_one()
 for record in cursor:
     print(record)
-#Call the delete_one() method by student_id 1010.
+# Call the delete_one() method by student_id 1010.
 cursor = collection.find_one()
 for record in cursor:
     print(record)
     result = collection.delete_one({"student_id": 1010})
-#Call the find() method and display the results to the terminal window.
+# Call the find() method and display the results to the terminal window.
 cursor = collection.find()
 
 for record in cursor:
